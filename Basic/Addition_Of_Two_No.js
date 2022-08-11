aa331d408ad3e49ca7e7ebd7af2485a0142eea4b
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-08-11 19:43:35 +0530

    Addition Of Two No

---
+++ Basic/Addition_Of_Two_No.js
@@ -0,0 +1,13 @@
+function main() {
+    var No1 = 11;
+    var No2 = 12;
+    var Ans = 0;
+    Ans = Addition(No1, No2);
+    console.log("Addir=tion Of Two Number Is:" + Ans);
+}
+function Addition(Value1, Value2) {
+    var ret = 0;
+    ret = Value1 + Value2;
+    return ret;
+}
+main();
