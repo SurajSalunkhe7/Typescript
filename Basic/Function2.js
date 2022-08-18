d52bd48caa6cef595cebea543c8302bb1cf4c6c6
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-08-18 06:58:39 +0530

    Function2

---
+++ Basic/Function2.js
@@ -0,0 +1,17 @@
+function Display(Value) {
+    console.log("Parameter is:" + Value);
+}
+var ans = 11;
+Display(ans);
+function main() {
+    var No1 = 12;
+    var Ans = 0;
+    Ans = Show(No1);
+    console.log("Parameter are" + Ans);
+}
+function Show(Value) {
+    var ret = 0;
+    ret = Value;
+    return ret;
+}
+main();
