90b41a40feef10714a3aba74b17026ff93795f34
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-08-23 19:46:55 +0530

    Increment

---
+++ Basic/increment.js
@@ -0,0 +1,12 @@
+function main() {
+    var a = 12;
+    var Ans = 0;
+    Ans = sun(a);
+    console.log("Return Value Is:" + Ans);
+}
+function sun(Value) {
+    var i = Value;
+    i++;
+    return i;
+}
+main();
