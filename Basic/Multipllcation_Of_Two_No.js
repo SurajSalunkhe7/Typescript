9778108a41fab3763db62cefe5eb5a5ea5dbdf84
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-08-24 15:53:45 +0530

    Multipllcation Of Two Number

---
+++ Basic/Multipllcation_Of_Two_No.js
@@ -0,0 +1,13 @@
+function main() {
+    var No1 = 5;
+    var No2 = 25;
+    var Ans = 0;
+    Ans = Multipllcation(No1, No2);
+    console.log("Multipllication Of Two Number:" + Ans);
+}
+function Multipllcation(Value1, Value2) {
+    var ret = 0;
+    ret = Value1 * Value2;
+    return ret;
+}
+main();
