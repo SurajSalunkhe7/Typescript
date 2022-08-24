90c1defeaff9291513f23693bfd2c3dead750c91
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-08-24 15:52:41 +0530

    Loop1

---
+++ Basic/Loop1.ts
@@ -0,0 +1,35 @@
+
+//Loop:for,while,do-while
+
+function Sequance():void
+{
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+}
+
+
+function Itration_For():void
+{
+    var i:number=0;
+    for(i=1;i<=5;i++)
+    {
+        console.log("Hello");
+    }
+}
+
+function Itration_while():void
+{
+    var i:number=1;
+    while(i<=5)
+    {
+        console.log("Hello");
+        i++;  
+    }
+
+}
+Sequance();
+Itration_For();
+Itration_while();
