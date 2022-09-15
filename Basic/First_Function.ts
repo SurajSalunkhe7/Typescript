e1c9f5eaa698ad600a27702caee6266c6a467418
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-09-15 20:05:07 +0530

    First Function

---
+++ Basic/First_Function.ts
@@ -0,0 +1,34 @@
+var Ret:number=0;
+
+//Regular Function
+function Addition1(No1:number,No2:number):number
+{
+    var Ans:number = 0;
+    Ans = No1+No2;
+    return Ans;
+}
+
+Ret = Addition1(10,11);
+console.log("Addition Of Two Numbers:"+Ret);
+
+//Anonymous Function(Function Without Name)
+var Addition2=function(No1:number,No2:number):number
+{
+    var Ans:number = 0;
+    Ans = No1+No2;
+    return Ans;
+}
+
+Ret = Addition2(10,11);
+console.log("Addition Of Two Numbers:"+Ret);
+
+//Fat arrow/Lambda/Arrow Function
+var Addition3 = (No1:number,No2:number):number=>
+{
+    var Ans:number = 0;
+    Ans = No1+No2;
+    return Ans;
+}
+
+Ret = Addition3(10,11);
+console.log("Addition Of Two Numbers:"+Ret);
